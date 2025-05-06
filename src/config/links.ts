6362c857eaa41653d6f49624cf31a06128723686
Vlad <vladka@webdevelop.pro>
2025-05-06 17:03:40 +0200

    fixed privacy policy link

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,5 +2,5 @@ export const links = {
   howItWorks: '#how-it-works',
   buyNow: 'https://buy.stripe.com/test_14kg1E3DNgBsg92288',
   terms: '/legal/terms-of-service',
-  privacy: '/legal/privacy-notice',
+  privacy: '/legal/privacy-policy',
 };
